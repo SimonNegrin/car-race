edaf2684479243ee05a5fe5a4b5fe493e83e3675
Simón Negrín <simon.negrin@gmail.com>
2023-12-11 22:40:02 +0000

    Add basic functionality

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,44 @@
 <script>
-  import Game from './lib/Game.svelte'
+  import { onMount } from 'svelte'
+  import Screen from './lib/Screen.svelte'
+
+  let isFullScreen = false
+
+  function toggleFullScreen() {
+    if (isFullScreen) {
+      document.exitFullscreen()
+    } else {
+      document.documentElement.requestFullscreen().then(() => {
+        window.screen.orientation.lock('landscape')
+      })
+    }
+    isFullScreen = !isFullScreen
+  }
 
 </script>
 
 <div class="app">
-  <Game />
+  <Screen />
+
+  <button
+    type="button"
+    class="full-screen-button"
+    on:click={toggleFullScreen}
+    >Full screen</button>
 </div>
 
 <style>
   .app {
+    position: relative;
     width: 100vw;
     height: 100vh;
-    background-color: darkred;
+    background-color: #222;
+  }
+  .full-screen-button {
+    position: absolute;
+    z-index: 10;
+    top: 1rem;
+    right: 1rem;
   }
 </style>
 
---
+++ src/lib/Screen.svelte
@@ -0,0 +1,85 @@
+<script>
+
+  import { orientation } from './services'
+  import Game from './Game.svelte'
+
+  let beta = 0
+  let keyLeft = false
+  let keyRight = false
+  let keyFaster = false
+  let keySlower = false
+  let angleThreshold = 7
+
+  $: updateOrientation($orientation)
+
+  function updateOrientation(orientation) {
+    if (orientation) {
+      beta = orientation.beta
+      keyLeft = orientation.beta > angleThreshold
+      keyRight = orientation.beta < -angleThreshold
+    }
+  }
+
+</script>
+
+<div class="screen-frame">
+  <div class="screen" style="transform: rotate({beta}deg);">
+    <Game
+      {keyLeft}
+      {keyRight}
+      {keyFaster}
+      {keySlower}
+    />
+  </div>
+  <button
+    type="button"
+    class="ctrl-btn brake-btn"
+    on:pointerdown={() => keySlower = true}
+    on:pointerup={() => keySlower = false}
+    />
+  <button
+    type="button"
+    class="ctrl-btn accelerator-btn"
+    on:pointerdown={() => keyFaster = true}
+    on:pointerup={() => keyFaster = false}
+    />
+</div>
+
+<style>
+
+  .screen-frame {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    background-color: #222;
+  }
+
+  .screen {
+    width: 100%;
+    height: 100%;
+    box-sizing: border-box;
+  }
+
+  .ctrl-btn {
+    position: absolute;
+    z-index: 10;
+    top: 0;
+    width: 10rem;
+    height: 100%;
+    border: none;
+    outline: none;
+    background-color: rgba(255, 255, 255, 0.189);
+
+    &:active {
+      background-color: rgba(255, 255, 255, 0.389);
+    }
+  }
+
+  .brake-btn {
+    left: 0;
+  }
+  .accelerator-btn {
+    right: 0;
+  }
+
+</style>
